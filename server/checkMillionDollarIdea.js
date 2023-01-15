6337321e257a2380d61dea52fbd8b1b7952b86da
Benjamin Wall <benwallmain@outlook.com>
2023-01-15 12:20:56 +0000

    Added logic to check if idea is worth a million dollars

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,13 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+  const { numWeeks, weeklyRevenue } = req.body;
+  const value = numWeeks * weeklyRevenue;
+  const isMillionDollarIdea = value >= 1000000 ? true : false;
+  if (isMillionDollarIdea) {
+    next();
+  } else {
+    res.status(400).send('Idea is not profitable!');
+  }
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
